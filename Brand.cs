b89df0d5026030d071f23a8dd30cc0cb039be98c
tungrg <tungrg07@gmail.com>
2025-07-04 18:01:25 +0700

    sua name brand

--- Brand.cs
+++ Brand.cs
@@ -9,11 +9,11 @@ namespace Slide6
     internal class Brand
     {
         public int id;
-        public string brandName;
+        public string name;
         public Brand(int id, string brandName)
         {
             this.id = id;
-            this.brandName = brandName;
+            this.name = brandName;
         }
     }
 }
